fae2b409d76170e4036a6e7f967d0e36eef031ab
__R D X-- <111151404+ShaquibKhanhub@users.noreply.github.com>
2023-05-01 01:46:33 +0530

    crypto-app

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Button, HStack ,Link} from '@chakra-ui/react'
+function Header() {
+  return <HStack p={'4'}
+  shadow={'base'}
+  bgColor={'blackAlpha.900'}>
+<Button variant={'unstyled'} color={'white'}>
+    <Link  to='/' >
+    Home
+    </Link>
+</Button>
+
+<Button variant={'unstyled'} color={'white'}>
+    <Link  to='/exchange' >
+   Exhanges
+    </Link>
+</Button>
+
+<Button variant={'unstyled'} color={'white'}>
+    <Link  to='/coins' >
+   Coins
+    </Link>
+</Button>
+  </HStack>
+}
+
+export default Header
